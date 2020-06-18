72310dbbadc74d62651fb9c00282370376db7b3c
BuianAlex <a.buyan@ukr.net>
2020-06-18 23:56:36 +0300

    f-commit

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' });
 
 // require and use "multer"...
 
@@ -11,11 +13,23 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
+  res.sendFile(process.cwd() + '/views/index.html');
+});
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get('/hello', function (req, res) {
+  res.json({ greetings: 'Hello, API' });
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (req.file) {
+    res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size,
+    });
+  } else {
+    res.status(400).send('file is not uploaded');
+  }
 });
 
 app.listen(process.env.PORT || 3000, function () {
